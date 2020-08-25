7ba6de64d8d5e0656f84b82a78b56e09d4191645
udofia2 <enalsy22@gmail.com>
2020-08-25 04:47:15 +0100

    user db design implemented

---
+++ api/model/citizen.model.js
@@ -0,0 +1,52 @@
+const { Schema } = require('mongoose')
+
+const citizenSchema = new Schema({
+    //Personal Data
+    fName: {
+        type: String,
+        required: true
+    },
+    lName: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    gender: {
+        type: String,
+        enum: [Male, Female, Transgender]
+
+    },
+    age: {
+        type: Number
+    },
+    DOB: {
+        type: Date
+    },
+    occupation: {
+        type: String
+    },
+    maritalStatus: {
+        type: String
+    },
+    //Natioal Data
+    nationality: {
+        type: String
+    },
+    state: String,
+    city: String,
+    LGA: String,
+    homeTown: String,
+    Religion: String, 
+
+    //Biological Data
+    weight: Number,
+    height: Number,
+    bloodGroup: String,
+
+    // Parental Data
+    FathersN: String
+})
