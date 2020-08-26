a27f4c77d2017798a1d6836a1a22218ff937fdb1
udofia2 <enalsy22@gmail.com>
2020-08-26 12:24:00 +0100

    comments added to the admin controller

--- api/controller/admin.Controller.js
+++ api/controller/admin.Controller.js
@@ -1,14 +1,31 @@
 const adminActions = (validationResult, Admin, bcrypt) => {
+  /**
+   * @param       GET /api/v1/admin
+   * @desc        Fectches list of Admins
+   * @access      Everyone can access
+   */
   const admins = async (req, res) => {
+    //Feteches all the admins from the database
     const admins = await Admin.find({});
     res.json(admins);
   };
 
+  /**
+   * @param       POST /api/v1/admin/login
+   * @desc        An admin login route
+   * @access      Everyone can access
+   */
   const login = (req, res) => {
     res.json("Admin login here");
   };
 
+  /**
+   * @param       POST /api/v1/admin/register
+   * @desc        An admin registeration route
+   * @access      Everyone can access
+   */
   const register = async (req, res) => {
+    //Validates form before registration
     try {
       const errors = validationResult(req);
       if (!errors.isEmpty()) {
@@ -17,16 +34,19 @@ const adminActions = (validationResult, Admin, bcrypt) => {
 
       const { name, email, password } = req.body;
 
+      //Checks if admin exist
       const isUsed = await Admin.findOne({ email });
 
       if (isUsed) return res.json(`${email} is already an Admin`);
 
+      //creates new admin
       const newAdmin = new Admin({
         name,
         email,
         password,
       });
 
+      //Encrypt admin password
       const salt = await bcrypt.genSalt(10);
       const hash = await bcrypt.hash(password, salt);
       newAdmin.password = hash;
@@ -39,6 +59,11 @@ const adminActions = (validationResult, Admin, bcrypt) => {
     }
   };
 
+  /**
+   * @param       GET /api/v1/:adminID
+   * @desc        Fectches single Admin
+   * @access      protected( only signed in admin can access)
+   */
   const admin = (req, res) => {
     res.json("Single admin");
   };
